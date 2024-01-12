e15ff1d7953e90cbe4244cad9e162bb02bd29452
demarb <demarcbrown01@gmail.com>
2024-01-12 17:12:29 -0500

    Semi-complete prooject dashboard minus table

--- app/project/[project-id]/page.tsx
+++ app/project/[project-id]/page.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import Image from 'next/image'
 
 const ProjectPage = () => {
   return (
-    <section className='w-4/5 mx-auto py-12'>
+    <section className='mx-auto py-2'>
 
       <div className=''>
-        <div>
-          <h2>Bugs</h2>
-          <div>
+        <div className='flex justify-between'>
+          <h2 className='text-3xl text-primary-green'>Bugs</h2>
+          <div className='flex'>
             <button type="button"
             //   key={}
             //   onClick={}
-              className="green_btn ">Sign In
+              className="rounded-md hover:border hover:border-primary-green "
+            >
+              <Image
+                className=""
+                src="/assets/icons/filter-60.png"
+                alt="Filter icon"
+                width={35}
+                height={35}
+                // priority
+              />    
             </button>
 
             <button type="button"
             //   key={}
             //   onClick={}
-              className="green_btn ">Sign In
+              className="green_btn mx-2">Submit New Bug
             </button>
           </div>
         </div>       
