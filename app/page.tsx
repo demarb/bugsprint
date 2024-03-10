0e52336529819f0e0830e54e1b7852eb9f967b9b
demarb <demarcbrown01@gmail.com>
2024-03-10 16:43:33 -0400

    Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from "next/link"
 // import {getData} from '@/utils/database'
 
 export default function Home() {
@@ -16,22 +17,57 @@ export default function Home() {
   
 
   return (
-    <section className="w-3/4 mx-auto flex flex-col items-center">
-        <h1 className='head_text text-center py-16'>Track Bugs For Your Next Software Project</h1>
+    <section className="">
 
-        <p className='text-center text-stone-800 md:py-8'>
-          {/* Report, Document, Assign & Track Bugs For Your Development Team */}
-          Streamline Your Development Workflow with Seamless Bug Reporting, Documentation, Assignment, and Tracking. Elevate Collaboration and Boost Productivity with Our Intuitive Bug Tracking Software.
-        </p>
-        
-        <Image
-          className=""
-          src="/assets/icons/down-arrow-50.png"
-          alt="Scroll down arrow"
-          width={50}
-          height={50}
-          // priority
-        />
+        <div className='w-3/4 h-screen mx-auto flex flex-col items-center py-2 lg:py-8'>
+          <h1 className='head_text text-center py-16'>Track Bugs For Your Next Software Project</h1>
+
+          <p className='text-center text-lg text-stone-800 md:py-8'>
+            {/* Report, Document, Assign & Track Bugs For Your Development Team */}
+            Streamline Your Development Workflow with Seamless Bug Reporting, Documentation, Assignment, and Tracking. Elevate Collaboration and Boost Productivity with Our Intuitive Bug Tracking Software.
+          </p>
+
+          <Link href="#desktop-preview" className="">
+                
+            <Image
+              className=""
+              src="/assets/icons/down-arrow-50.png"
+              alt="Scroll down arrow"
+              width={50}
+              height={50}
+              // priority
+            />
+          </Link>
+          
+        </div>
+
+        <div className='bg-primary-green py-2 lg:py-8'>
+
+          {/* <div id='' className='mx-auto w-11/12 lg:w-3/4'>
+            <Image
+              className=""
+              src="/assets/images/Desktop green preview (2).png"
+              alt="Desktop preview"
+              width={1920}
+              height={1080}
+              // priority
+            />
+          </div> */}
+
+          <div id='desktop-preview' className='mx-auto w-11/12 lg:w-3/4'>
+            <Image
+              className=""
+              src="/assets/images/Desktop green preview (2).png"
+              alt="Desktop preview"
+              width={1920}
+              height={1080}
+              // priority
+            />
+          </div>
+          
+        </div>
+
+
     </section>
   )
 }
