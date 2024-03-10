3d2827a6de03bfcbc11bd5e049ccef422b32e8a8
demarb <demarcbrown01@gmail.com>
2024-03-10 17:42:07 -0400

    Change styling of home

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <section className="">
 
-        <div className='w-3/4 h-screen mx-auto flex flex-col items-center py-2 lg:py-8'>
+        <div className='w-3/4 h-screen mx-auto flex flex-col justify-normal items-center py-2 lg:py-4'>
           <h1 className='head_text text-center py-16'>Track Bugs For Your Next Software Project</h1>
 
           <p className='text-center text-lg text-stone-800 md:py-8'>
@@ -27,7 +27,7 @@ export default function Home() {
             Streamline Your Development Workflow with Seamless Bug Reporting, Documentation, Assignment, and Tracking. Elevate Collaboration and Boost Productivity with Our Intuitive Bug Tracking Software.
           </p>
 
-          <Link href="#desktop-preview" className="">
+          <Link href="#preview" className="">
                 
             <Image
               className=""
@@ -54,15 +54,45 @@ export default function Home() {
             />
           </div> */}
 
-          <div id='desktop-preview' className='mx-auto w-11/12 lg:w-3/4'>
+          <div id='preview' className='mx-auto w-11/12 lg:w-3/4 max-md:hidden'>
+            <div className='py-16 text-5xl font-extrabold leading-[1.15] text-white sm:text-6xl text-center'>
+              <h1 className=''>Create Projects.</h1>
+              <h1 className=''>Join Shared Projects.</h1>
+              <h1 className=''>Add and Track Bugs.</h1>
+              <h1 className=''>Modify Team Permissions.</h1>
+            </div>
+            
             <Image
               className=""
-              src="/assets/images/Desktop green preview (2).png"
+              src="/assets/images/Desktop preview.png"
               alt="Desktop preview"
               width={1920}
               height={1080}
-              // priority
             />
+
+          </div>
+
+          <div id='preview' className='mx-auto w-11/12 lg:w-3/4 md:hidden'>
+            <div className='py-16 text-5xl font-extrabold leading-[1.15] text-white sm:text-6xl text-center'>
+              <h1 className=''>Create Projects.</h1>
+              <h1 className=''>Join Shared Projects.</h1>
+             </div>
+            
+            <div className=''>
+              <Image
+                className=""
+                src="/assets/images/Mobile preview.png"
+                alt="Mobile preview"
+                width={1080}
+                height={1920}
+              />
+            </div>
+
+            <div className='py-16 text-5xl font-extrabold leading-[1.15] text-white sm:text-6xl text-center'>
+              <h1 className=''>Add and Track Bugs.</h1>
+              <h1 className=''>Modify Team Permissions.</h1>
+             </div>
+            
           </div>
           
         </div>
